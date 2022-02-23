4f5e10723a675773fa4cedd79998944daf32425f
Victor Caruso <vic.caruso@gmail.com>
2022-02-23 13:47:52 -0800

    set up GameProvider.js and moved state there from App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import initialCards from './cards-data';
 import Player from './Player';
-import CardList from './CardList';
 import { useState } from 'react';
 import ExecutePassButton from './ExecutePassButton';
+import CardList from './CardList';
+import { useGameContext } from './GameProvider';
 
 function App() {
-  const [deck, setDeck] = useState(initialCards);
-  const [playerOneHand, setPlayerOneHand] = useState([]);
-  const [selectedCard, setSelectedCard] = useState();
-  const [playerTwoHand, setPlayerTwoHand] = useState([]);
-  const [playerThreeHand, setPlayerThreeHand] = useState([]);
-  const [from, setFrom] = useState('deck');
-  const [to, setTo] = useState(1);
+
+  const { deck, setDeck,
+    playerOneHand, setPlayerOneHand,
+    selectedCard, setSelectedCard,
+    playerTwoHand, setPlayerTwoHand,
+    playerThreeHand, setPlayerThreeHand,
+    from, setFrom,
+    to, setTo } = useGameContext();
 
   function findCardIndex(value, suit, cards) {
     return cards.findIndex(card => card.value === value && card.suit === suit);
