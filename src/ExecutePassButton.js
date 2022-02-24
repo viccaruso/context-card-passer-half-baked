d363b749ed9217b31f92e5c7eb7ff1f7e0e25ef1
Victor Caruso <vic.caruso@gmail.com>
2022-02-23 16:39:27 -0800

    refactored ExecutePassButton to use context

--- src/ExecutePassButton.js
+++ src/ExecutePassButton.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Card from './Card';
+import { useGameContext } from './GameProvider';
 
-export default function ExecutePassButton({ passCard, from, to, selectedCard }) {
+export default function ExecutePassButton({ passCard }) {
+  const { from, to, selectedCard } = useGameContext();
   return (
     <div className='execute-button' onClick={() => passCard(selectedCard)}>
         Pass <Card card={selectedCard} /> from {from} to {to}
